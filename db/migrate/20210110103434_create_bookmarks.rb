ab6d8eabbaac9b475385d166c0b6712d989543a0
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-10 19:38:44 +0900

    update: bookmarkモデルのバリデーション・カラムの追加#17

--- db/migrate/20210110103434_create_bookmarks.rb
+++ db/migrate/20210110103434_create_bookmarks.rb
@@ -3,6 +3,8 @@ class CreateBookmarks < ActiveRecord::Migration[6.0]
     create_table :bookmarks do |t|
 
       t.timestamps
+      t.references :user,        null: false, foreign_key: true
+      t.references :post,        null: false, foreign_key: true
     end
   end
 end
