67219b931ba66bf398b68754e37afbe75541d068
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-10 19:29:50 +0900

    update: likeモデルのバリデーション・カラムの追加#17

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,2 +1,8 @@
 class Like < ApplicationRecord
+  #アソシエーション
+  belongs_to :user
+  belongs_to :post
+  #バリデーション
+  validates_uniqueness_of :post_id, scope: :user_id
+
 end
--- db/migrate/20210110100145_create_likes.rb
+++ db/migrate/20210110100145_create_likes.rb
@@ -3,6 +3,8 @@ class CreateLikes < ActiveRecord::Migration[6.0]
     create_table :likes do |t|
 
       t.timestamps
+      t.references :user,        null: false, foreign_key: true
+      t.references :post,        null: false, foreign_key: true
     end
   end
 end
