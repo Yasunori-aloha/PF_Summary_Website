f28226566bc27a89c35faa9b660f175e5cfd1127
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-10 15:16:50 +0900

    update: Userモデルバリデーション・テーブル作成#17

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         #アソシエーション
+         has_many :posts
+         has_many :comments
+         has_many :likes
+         has_many :bookmarks
+         #バリデーション
+         with_options format: { with: /\A[a-zA-Z0-9]+\z/} do
+          validates :password
+          validates :password_confirmation
+         end
+         validates :nickname, presence: true
 end
