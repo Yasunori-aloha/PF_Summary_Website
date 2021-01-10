64ae4e91ac280e7fb052fc8eb8b10bc5f7036c1e
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-10 17:46:10 +0900

    update: tagモデルのバリデーション・カラム#17

--- db/migrate/20210110083228_create_tags.rb
+++ db/migrate/20210110083228_create_tags.rb
@@ -3,6 +3,8 @@ class CreateTags < ActiveRecord::Migration[6.0]
     create_table :tags do |t|
 
       t.timestamps
+      t.string :tag_name,        null: false
+      t.references :post,        null: false, foreign_key: true
     end
   end
 end
