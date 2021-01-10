4c11ca1ec7e16d8756f46f66624dbf22c345e4fc
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-10 17:32:06 +0900

    update:commentモデルのバリデーション＆カラム#17

--- db/migrate/20210110080327_create_comments.rb
+++ db/migrate/20210110080327_create_comments.rb
@@ -3,6 +3,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
     create_table :comments do |t|
 
       t.timestamps
+      t.references :user,    null: false, foreign_key: true
+      t.references :post,    null: false, foreign_key: true
     end
   end
 end
