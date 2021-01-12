5bef597243e88ff93b6648a51d57f074574ead08
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-12 19:04:45 +0900

    update: Tagモデルの単体テストコード#17

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tag) { FactoryBot.create(:tag) }
+
+  describe 'タグ付機能' do
+    context '機能がうまくいくとき' do
+      it 'tag_nameが入力されていればタグ付できる' do
+        expect(tag).to be_valid
+      end
+    end
+
+    context '機能がうまくいかないとき' do
+      it 'tag_nameが空だとタグ付できない' do
+        tag.tag_name = ""
+        tag.valid?
+        expect(tag.errors.full_messages).to include 'Tag nameを入力してください'
+      end
+    end
+  end
 end
