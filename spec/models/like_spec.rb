94bf99af846b80c0020c2c00c1d28cddda762d97
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-12 22:15:01 +0900

    update: Likeモデルの単体テストコード#17

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user) }
+  let(:post) { FactoryBot.create(:post) }
+  let(:like) { FactoryBot.create(:like, user_id: user.id, post_id: post.id) }
+
+  describe 'いいね機能' do
+    context 'いいねができる場合' do
+      it 'ログインユーザーによる1投稿に1いいね' do
+        expect(like).to be_valid
+      end
+    end
+
+    context 'いいねができない場合' do
+      it '1つの投稿に対して2回以上のいいねはできない' do
+        like.save
+        another_like = FactoryBot.build(:like, user_id: user.id, post_id: post.id)
+        another_like.valid?
+        expect( another_like.errors.full_messages).to include 'Postはすでに存在します'
+      end
+
+      it 'ログインしていないユーザーはいいねできない' do
+        like.user = nil
+        like.valid?
+        expect(like.errors.full_messages).to include 'Userを入力してください'
+      end
+    end
+  end
 end
