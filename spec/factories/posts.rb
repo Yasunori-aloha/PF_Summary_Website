03022dbb097acedc7a37b4049d58870bf29f2f35
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-12 16:39:16 +0900

    update: Postモデルの単体テストコード#17

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :post do
-    
+    name     { 'ポートフォリオサイト' }
+    content  { Faker::Lorem.characters(number: 500) }
+    url      { Faker::Internet.url }
+
+    association :user
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:post) { FactoryBot.create(:post)}
+
+  describe '投稿機能' do
+    context '投稿ができるとき' do
+      it '全ての項目の入力が存在すれば投稿できる' do
+        expect(post).to be_valid
+      end
+    end
+
+    context '投稿ができないとき' do
+      it 'nameが空だと投稿できない' do
+        post.name = ""
+        post.valid?
+        expect(post.errors.full_messages).to include 'Nameを入力してください'
+      end
+
+      it 'contentが空だと投稿できない' do
+        post.content = ""
+        post.valid?
+        expect(post.errors.full_messages).to include 'Contentを入力してください'
+      end
+
+      it 'contentが501文字以上だと投稿できない' do
+        post.content = "ア" * 501
+        post.valid?
+        expect(post.errors.full_messages).to include 'Contentは500文字以内で入力してください'
+      end
+
+      it 'URLが空だと投稿できない' do
+        post.url = ""
+        post.valid?
+        expect(post.errors.full_messages).to include 'Urlを入力してください'
+      end
+
+      it 'URLの始めにHTTPが書かれていなければ投稿できない' do
+        post.url = "fay-rutherford.com/digna_littel"
+        post.valid?
+        expect(post.errors.full_messages).to include 'Urlは不正な値です'
+      end
+    end
+  end
 end
