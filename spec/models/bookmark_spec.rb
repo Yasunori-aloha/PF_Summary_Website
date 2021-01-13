e208f1361bfd0030c2af0309f912c525abbe36a1
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-13 16:30:58 +0900

    update: bookmarkテストファイルの修正#17

--- spec/models/bookmark_spec.rb
+++ spec/models/bookmark_spec.rb
@@ -5,25 +5,31 @@ RSpec.describe Bookmark, type: :model do
   let(:post) { FactoryBot.create(:post) }
   let(:bookmark) { FactoryBot.build(:bookmark, user: user, post: post) }
 
-  describe 'いいね機能' do
-    context 'いいねができる場合' do
-      it 'ログインユーザーによる1投稿に1いいね' do
+  describe 'ブックマーク機能' do
+    context 'ブックマークができる場合' do
+      it 'ログインユーザーによる1投稿に1回しかできない' do
         expect(bookmark).to be_valid
       end
     end
 
-    context 'いいねができない場合' do
-      it '1つの投稿に対して2回以上のいいねはできない' do
+    context 'ブックマークができない場合' do
+      it '1つの投稿に対して2回以上はできない' do
         bookmark.save
         another_bookmark = FactoryBot.build(:bookmark, user: user, post: post)
         another_bookmark.valid?
         expect( another_bookmark.errors.full_messages).to include 'Postはすでに存在します'
       end
 
-      it 'ログインしていないユーザーはいいねできない' do
+      it 'ログインしていないユーザーはブックマークできない' do
         bookmark.user = nil
         bookmark.valid?
         expect(bookmark.errors.full_messages).to include 'Userを入力してください'
+      end
+
+      it '存在しない投稿に対してのブックマーク' do
+        bookmark.post = nil
+        bookmark.valid?
+        expect(bookmark.errors.full_messages).to include 'Postを入力してください'
       end
     end
   end
