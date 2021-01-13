740a1979cd40ee899669b5f3fcbd535dcb147680
yamamurodaisuke <daisuke0308@icloud.com>
2021-01-13 20:58:51 +0900

    update: likeテストファイルの再修正#17

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  let(:user) { FactoryBot.create(:user) }
-  let(:post) { FactoryBot.create(:post) }
-  let(:like) { FactoryBot.build(:like, user_id: user.id, post_id: post.id) }
+  let(:like) { FactoryBot.build(:like) }
 
   describe 'いいね機能' do
     context 'いいねができる場合' do
@@ -15,7 +13,7 @@ RSpec.describe Like, type: :model do
     context 'いいねができない場合' do
       it '1つの投稿に対して2回以上のいいねはできない' do
         like.save
-        another_like = FactoryBot.build(:like, user_id: user.id, post_id: post.id)
+        another_like = FactoryBot.build(:like, user_id: like.user_id, post_id: like.post_id)
         another_like.valid?
         expect( another_like.errors.full_messages).to include 'Postはすでに存在します'
       end
